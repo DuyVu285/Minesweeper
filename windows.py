9445e48ad8f889ebf7c8051199cb78f26317d8f7
DuyVu285 <80942615+DuyVu285@users.noreply.github.com>
2022-05-25 14:16:52 +0700

    Update windows.py

--- windows.py
+++ windows.py
@@ -1,8 +1,27 @@
 from tkinter import *
 
-# create game window
+# Create game window
 root = Tk()
+root.configure(bg = 'black')
 root.geometry('1440x720')
 root.title('Minesweeper Game')
 root.resizable(True, True)
+
+# Divide the game window into sections
+top_frame = Frame(
+    root,
+    bg = 'red', # Change later to black
+    width = 1440,
+    height = 180
+)
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(
+    root,
+    bg = 'blue', # Change later to black
+    width = 360,
+    height = 540
+)
+left_frame.place(x=0, y=200)
+# Divide the game window into sections
 root.mainloop()
